50820aeeda1ede80550408776d9371f38fc2f9e8
Leonardo81 <leonardoeinstein2000@gmail.com>
2021-10-24 15:12:56 +0700

    dplasma init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ import routes from './routes';
 import './App.css';
 import { ConnectedRouter } from 'connected-react-router';
 // import { PersistGate } from 'redux-persist/integration/react';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+initializeApp(firebaseConfig);
 
 function App() {
   return (
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 
 const Home = props => {
+  const [auth, setAuth] = useState();
+  const [token, setToken] = useState();
+
+  useEffect(()=> {
+    const au = getAuth();
+    console.log(au);
+    setAuth(au);
+  }, [])
+  const clicked = () => {
+    signInWithEmailAndPassword(auth, 'leonardo81@ui.ac.id', 'pokmnjiuhb').then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(userCredential);
+      setToken(user.accessToken)
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  }
+
+  const signup = () => {
+    createUserWithEmailAndPassword(auth, 'leonardo81@ui.ac.id', 'pokmnjiuhb').then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(userCredential);
+      setToken(user.accessToken)
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+  }
   return (
-    <div>Halo</div>
+    <div>
+      <div>Halo</div>
+      <button onClick={clicked}>CLICK</button>
+      <button onClick={signup}>SIGNUP</button>
+      <br></br>
+      <p>{token}</p>
+    </div>
   )
 };
 

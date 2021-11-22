3ce3ed31319c39413d7beebfb4a3f7bee131039a
Leo :) <leonardo.leonardo@ibm-jti.com>
2021-11-22 21:31:47 +0700

    ganti server

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,6 +1,6 @@
-export const serverlessBaseUrl = "https://asia-southeast2-ta-leonardo.cloudfunctions.net";
+export const serverlessBaseUrl = "https://us-east4-new-ta-leonardo.cloudfunctions.net";
 export const monolithBaseUrl = "https://dplasma.leonardo.web.id";
-export const hybridBaseUrl = "https://ta-leonardo.et.r.appspot.com";
+export const hybridBaseUrl = "https://new-ta-leonardo.uk.r.appspot.com";
 
 const server = JSON.parse(localStorage.getItem('server'));
 export const serverList = ["", monolithBaseUrl, hybridBaseUrl, serverlessBaseUrl];

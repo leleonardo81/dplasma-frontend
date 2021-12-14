89816e4a9c62bd11ae89ae6164fbf2f7377387f2
Leo :) <leonardo.leonardo@ibm-jti.com>
2021-12-14 18:27:40 +0700

    test demo

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,6 +1,9 @@
-export const serverlessBaseUrl = "https://us-east4-new-ta-leonardo.cloudfunctions.net";
-export const monolithBaseUrl = "https://dplasma.leonardo.web.id";
-export const hybridBaseUrl = "https://new-ta-leonardo.uk.r.appspot.com";
+// export const serverlessBaseUrl = "https://us-east4-new-ta-leonardo.cloudfunctions.net";
+// export const monolithBaseUrl = "https://dplasma.leonardo.web.id";
+// export const hybridBaseUrl = "https://new-ta-leonardo.uk.r.appspot.com";
+export const serverlessBaseUrl = "https://us-east4-kafka-cluster-1.cloudfunctions.net";
+export const monolithBaseUrl = "http://34.150.199.44";
+export const hybridBaseUrl = "https://kafka-cluster-1.uc.r.appspot.com";
 
 const server = JSON.parse(localStorage.getItem('server'));
 export const serverList = ["", monolithBaseUrl, hybridBaseUrl, serverlessBaseUrl];
